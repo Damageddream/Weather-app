693f406e46405c04b4db32e209dcfb29a210ba7d
Damageddream <marcan.grab@gmail.com>
2023-04-22 23:16:12 +0200

    security commit

--- src/notes.js
+++ src/notes.js
@@ -50,7 +50,7 @@ const weatherIcons = {
     'Moderate or heavy snow with thunder': 395
   };
 
-  const sunnyWeather = ['Clear', 'Sunny'];
+const sunnyWeather = ['Clear', 'Sunny'];
 const cloudyWeather = ['Partly cloudy', 'Cloudy', 'Overcast', 'Mist', 'Fog', 'Freezing fog'];
 const rainyWeather = ['Patchy rain possible', 'Patchy light drizzle', 'Light drizzle', 'Freezing drizzle', 'Heavy freezing drizzle', 'Patchy light rain', 'Light rain', 'Moderate rain at times', 'Moderate rain', 'Heavy rain at times', 'Heavy rain', 'Light freezing rain', 'Moderate or heavy freezing rain', 'Light rain shower', 'Moderate or heavy rain shower', 'Torrential rain shower', 'Patchy light rain with thunder', 'Moderate or heavy rain with thunder'];
 const snowyWeather = ['Patchy snow possible', 'Patchy sleet possible', 'Patchy freezing drizzle possible', 'Blowing snow', 'Blizzard', 'Light sleet', 'Moderate or heavy sleet', 'Patchy light snow', 'Light snow', 'Patchy moderate snow', 'Moderate snow', 'Patchy heavy snow', 'Heavy snow']
